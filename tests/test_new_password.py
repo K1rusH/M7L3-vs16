92dbd612bbc46110035e56f102b61340ed2acce0
Timofey20101958 <tzologin@internet.ru>
2026-06-21 21:11:05 +0700

    Update test_new_password.py

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -15,3 +15,9 @@ def test_password_characters():
 Тест, что длина пароля соответствует заданной
 Тест, что два сгенерированных подряд пароля различаются
 """
+
+def test_password_lenght():         
+    assert (password_length(12)) >= 8
+
+def test_password_correct():
+    assert generate_password != 11111111 or 22222222 or 33333333 or 44444444 or 55555555 or 66666666 or 77777777 or 88888888 or 99999999 or 12345678 or 23456789 or 34567891 or 45678912 or 56789123 or 67891234 or 78912345 or 89123456 or 91234567
