f66954010168fc69f0417498194b19284b019b77
ilyagolovin2011 <ilyagolovin2011@gmail.com>
2026-06-21 18:49:14 +0500

    Добавил 2 теста

---
+++ tests/test_new_password.py
@@ -0,0 +1,21 @@
+import string
+from password.new_password import generate_password
+
+def test_password_characters():
+    """Тест, что при генерации используются только допустимые символы"""
+    valid_characters = string.ascii_letters + string.digits + string.punctuation
+    password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
+    for char in password:
+        assert char in valid_characters
+
+"""
+Допиши еще один тест из предложенных. Или придумай свой.
+Если сможешь написать больше, то будет круто!
+
+Тест, что длина пароля соответствует заданной
+Тест, что два сгенерированных подряд пароля различаются
+"""
+def test_password_length():
+    assert len(generate_password(12)) == 12
+def test_passwords_differ():
+    assert generate_password(12) != generate_password(12)
