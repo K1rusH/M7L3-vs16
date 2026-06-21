b2e4062873fc31533a1922d137799304c0233c82
Alex-ali0 <alexalekov7@gmail.com>
2026-06-21 18:48:21 +0500

    добавил 2 теста

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,7 +8,16 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
+
+def test_len_password():
+    password = generate_password(42)
+    assert len(password) == 42, "Длина не соответсвует заданной"
+
+def test_two_passwords():
+    password_1 = generate_password(12)
+    password_2 = generate_password(12)
+    assert password_1 != password_2, "Пароли одинаковые"
+    """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
 
